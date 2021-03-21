37e1535001f7df3cbef3a8ca96e4d238c44c6c3e
PryakhinMS <pryakhin.ms@gmail.com>
2021-03-21 18:32:01 +0400

    Поправлен движок игр, игра исправлена для работы с движком

--- src/brainCalcLogic.js
+++ src/brainCalcLogic.js
@@ -1,23 +1,16 @@
-import readlineSync from 'readline-sync';
+// import readlineSync from 'readline-sync';
 import getRandomNum from './getRandomNum.js';
 
-export default () => {
-  console.log('What is the result of the expression?');
-  for (let i = 0; i < 3; i += 1) {
-    const num1 = getRandomNum(20);
-    const num2 = getRandomNum(20);
-    const expression = {
-      0: { msg: `Question: ${num1}+${num2}`, value: num1 + num2 },
-      1: { msg: `Question: ${num1}-${num2}`, value: num1 - num2 },
-      2: { msg: `Question: ${num1}*${num2}`, value: num1 * num2 },
-    };
-    console.log(expression[num1 % 3].msg);
-    const answer = readlineSync.question('Your answer ');
-    if (expression[num1 % 3].value !== parseInt(answer, 10)) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${expression[num1 % 3].value}'.`);
-      return false;
-    }
-    console.log('Correct!');
+export default (mode) => {
+  if (mode === 'GENERATE_TASK') {
+    return 'What is the result of the expression?';
   }
-  return true;
+  const num1 = getRandomNum(20);
+  const num2 = getRandomNum(20);
+  const expression = [
+    [`${num1}+${num2}`, num1 + num2],
+    [`${num1}-${num2}`, num1 - num2],
+    [`${num1}*${num2}`, num1 * num2],
+  ];
+  return expression[num1 % 3];
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import readlineSync from 'readline-sync';
 import brainCalcLogic from './brainCalcLogic.js';
 
-const gamePlot = (game) => () => {
+const gameEngine = (game) => () => {
   console.log('Welcome to the Brain Games!');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
+  console.log(game('GENERATE_TASK'));
+  for (let i = 0; i < 3; i += 1) {
+    const [question, answer] = game();
+    console.log(`Question: ${question}`);
+    const userAnswer = parseInt(readlineSync.question('Your answer: '), 10);
+    if (answer !== userAnswer) {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
+      console.log(`Let's try again, ${playerName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
   // Запускаем игру. В игре есть цикл. Игра возвращает результат: true или false;
-  const result = game();
-  const finalMsg = result ? `Congratulations, ${playerName}!` : `Let's try again, ${playerName}!`;
-  console.log(finalMsg);
+  console.log(`Congratulations, ${playerName}!`);
 };
 
-export const brainCalc = gamePlot(brainCalcLogic);
+export const brainCalc = gameEngine(brainCalcLogic);
 export const a = 5;
